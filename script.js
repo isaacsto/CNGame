a4d9ed6e8a4fbdc44b4509888be13632d884079c
Isaac Stofko <istofko@contractornation.com>
2024-03-21 14:38:25 -0400

    popup

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function setupPopup() {
+    function openPopup() {
+        document.getElementById("popup").style.display = "block";
+    }
+
+    function closePopup() {
+        document.getElementById("popup-basement").style.display = "none";
+    }
+
+    function handleAreaClick() {
+        openPopup();
+    }
+
+document.getElementById("oval-8708").addEventListener("click", handleAreaClick);
+
+document.getElementById("close").addEventListener("click", closePopup);
+}
+
+document.addEventListener("DOMContentLoaded", setupPopup);
